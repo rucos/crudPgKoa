86121957abb7608d8bfb24a55809db2823941f3e
kirill <kosaev-kira@mail.ru>
2019-12-23 22:37:36 +0300

    type orm init

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
+import 'reflect-metadata'
 import app from './app'
 import config from './config'
+import { createConnection } from 'typeorm'
 
-const { port } = config
+const { port } = config;
 
-app.listen(port, () => {
-	console.log(`Server listening on port: ${port}`)
-})
+(async () => {
+	try {
+		await createConnection()
+		console.log('Database connect success')
+
+
+		app.listen(port, () => {
+			console.log(`Server listening on port: ${port}`)
+		})
+	} catch (error) {
+		console.error(error)
+	}
+})()
